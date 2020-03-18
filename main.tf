aef41c1ce6ddc0ed1d7012ed76b7a294fdc12c3e
Daniel Motles <seltom.dan@gmail.com>
2020-03-18 12:36:21 -0700

    Add aws_instance resources for all nodes in Qumulo cluster

--- main.tf
+++ main.tf
@@ -6,3 +6,30 @@
  * information or intellectual property contained herein is strictly forbidden,
  * unless separate prior written permission has been obtained from Qumulo, Inc.
  */
+
+resource "aws_instance" "leader_node" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  tags                   = merge({ Name = "${var.cluster_name} 1" }, var.tags)
+
+  # This user_data will be used to instruct the leader_node to form a cluster
+  # with the other nodes.
+  user_data = jsonencode({
+    node_ips     = aws_instance.node.*.private_ip
+    cluster_name = var.cluster_name
+  })
+}
+
+resource "aws_instance" "node" {
+  count = var.node_count - 1
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.security_group_ids
+  tags                   = merge({ Name = "${var.cluster_name} ${count.index + 1}" }, var.tags)
+}
