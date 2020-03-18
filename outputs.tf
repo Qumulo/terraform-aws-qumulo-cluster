bd858f6bb47426477ab89be138e335c57156fc6b
Daniel Motles <seltom.dan@gmail.com>
2020-03-18 14:56:10 -0700

    Add the temporary admin password as an output for the module

--- outputs.tf
+++ outputs.tf
@@ -21,3 +21,8 @@ output "public_ips" {
   value       = local.all_nodes.*.public_ip
   description = "EC2 instance public IPs (if set)"
 }
+
+output "temp_admin_password" {
+  value       = aws_instance.leader_node.id
+  description = "Temporary admin password for the Web UI and API"
+}
