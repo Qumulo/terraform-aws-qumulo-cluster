19a70546611c53ef94f383c40baed4a61b303563
Daniel Motles <seltom.dan@gmail.com>
2020-03-18 11:19:04 -0700

    Add minimum set of empty Terraform files

---
+++ variables.tf
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2019 Qumulo, Inc. All rights reserved.
+ *
+ * NOTICE: All information and intellectual property contained herein is the
+ * confidential property of Qumulo, Inc. Reproduction or dissemination of the
+ * information or intellectual property contained herein is strictly forbidden,
+ * unless separate prior written permission has been obtained from Qumulo, Inc.
+ */
